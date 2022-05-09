447efc2bd654cb84a817a6e8229325f5187e8a36
Daniel Esquivel-Reynoso <85596108+DanielEsquivelReynoso@users.noreply.github.com>
2022-05-09 14:45:51 -0700

    Update ProfilePhoto.js

--- components/ProfilePhoto.js
+++ components/ProfilePhoto.js
@@ -2,16 +2,11 @@ import React from 'react';
 import Head from 'next/head';
 
 const ProfilePhoto = () => {
-console.log('ProfilePhoto being rendered')
   return (
-    <div>
-      <main>
-        <div className="flex justify-left items-center">
-          <button className="bg-limed-spruce text-white w-[60px] h-[60px] rounded-[30px] ml-[25px] mt-[40px]">
-          </button>
-          <h1 className="font-sans-['Roboto'] text-[28px] text-white text-4xl text-bold mt-9 ml-[10px]">Full Name</h1>
-        </div>
-      </main>
+    <div className="flex justify-left items-center">
+      <button className="bg-limed-spruce text-white w-[60px] h-[60px] rounded-[30px] ml-[25px] mt-[40px]">
+      </button>
+      <h1 className="font-sans-['Roboto'] text-[28px] text-white text-4xl text-bold mt-9 ml-[10px]">Full Name</h1>
     </div>
   );
 };
