61bc32077a0b520bd11e3e40fd8caffb659e2029
Daniel Esquivel-Reynoso <85596108+DanielEsquivelReynoso@users.noreply.github.com>
2022-05-09 14:47:13 -0700

    Update SignOut.js

--- components/SignOut.js
+++ components/SignOut.js
@@ -2,20 +2,9 @@ import React from 'react';
 import Head from 'next/head';
 
 const SignOut = () => {
-  console.log('SignOut being rendered')
   return (
-    <div>
-      <Head>
-        <title>Sign Out Button</title>
-        <meta name="description" content="Genereted by create next app"/>
-        <link rel="icon" href="/images/favicon.ico" />
-      </Head>
-      <main>
-
-      <div className="flex justify-center items-end">
-        <button className="text-white bg-limed-spruce px-10 w-[87.7%] h-12 rounded-[14px] mt-3">Sign Out</button>
-      </div>
-      </main>
+    <div className="flex justify-center items-end">
+      <button className="text-white bg-limed-spruce px-10 w-[87.7%] h-12 rounded-[14px] mt-3">Sign Out</button>
     </div>
   );
 };
