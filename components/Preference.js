6e5d33912d9d873fbdc41849b7f97921363b8b8e
Daniel Esquivel-Reynoso <85596108+DanielEsquivelReynoso@users.noreply.github.com>
2022-05-09 14:44:37 -0700

    Update Preference.js

--- components/Preference.js
+++ components/Preference.js
@@ -2,23 +2,17 @@ import React from 'react';
 import Head from 'next/head';
 
 const Preference = (props) => {
-console.log('Preference being rendered')
-const pref = props.preference
+  
+  const pref = props.preference;
   return (
-    <div>
-      <main>
-        <div className="flex justify-left items-center text-white">
-          <div className="w-[stretch] mt-2">
-            <div className="text-[grey]">{pref.pref}</div>
-            <div>{pref.choice[0]}</div>
-          </div>
-          <button className="bg-[grey] w-[90px] h-[25px] rounded-[12px]">Change</button>
-        </div>
-      </main>
+    <div className="flex justify-left items-center text-white">
+      <div className="w-[stretch] mt-2">
+        <div className="text-[grey]">{pref.pref}</div>
+        <div>{pref.choice[0]}</div>
+      </div>
+      <button className="bg-[grey] w-[90px] h-[25px] rounded-[12px]">Change</button>
     </div>
   );
 };
-
-
 
 export default Preference;
