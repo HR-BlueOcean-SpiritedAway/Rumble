340608790e18c75305330953caf14863778ef172
Dennis Cao <dennisrcao@gmail.com>
2022-05-11 15:15:00 -0700

    cleaned up - removed dummy data + console.logs

--- pages/Page-J-Restaurant.js
+++ pages/Page-J-Restaurant.js
@@ -3,34 +3,6 @@ import { useState, useEffect } from 'react';
 import { useRouter} from 'next/router'
 import axios from 'axios';
 
-//Dummy data
-const restaurantTest = {
-  cuisine: "Italian",
-  zip_code: 94117,
-  addres: "311 Divisadero St",
-  restaurantName: "Ragazza",
-  priceRange: "$$",
-  state: "CA",
-  dishes: [
-      {
-          photoURL: "https://s3-media0.fl.yelpcdn.com/bphoto/-vpD8rR9-v61LpTG9tXiWg/o.jpg",
-          name: "Amatriciana"
-      },
-      {
-          name: "Moto",
-          photoURL: "https://s3-media0.fl.yelpcdn.com/bphoto/9_ZCRhfIUAsj-oeXFFidkQ/o.jpg"
-      },
-      {
-          photoURL: "https://s3-media0.fl.yelpcdn.com/bphoto/yvvLJoKtaSr7z4IUvViPKg/o.jpg",
-          name: "Bianca"
-      }
-  ],
-  description: "Lorem ipsum dolor sit amet",
-  city: "San Francisco"
-}
-
-
-
 export default function SingleRestaurant(props) {
   const router= useRouter();
   const [restaurant, setRestaurant] = useState([]);
@@ -43,10 +15,6 @@ export default function SingleRestaurant(props) {
       })
       .catch(console.error);
   }, [router.query.name]);
-
-  console.log('restarant state is', restaurant);
-  console.log('length', restaurant.length);
-
   return (
     <>
     {
