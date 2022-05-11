f6e9bda0b84a0850be5ee81cb3fdb4ea31031951
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-05-10 21:32:35 -0700

    feat: (almost) completes browse restaurants (Page H)

--- pages/Page-H-BrowseRestaurants.js
+++ pages/Page-H-BrowseRestaurants.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 
 import BackButton from '../components/Backbtn';
 import SearchBar from '../components/SearchBar';
@@ -10,6 +11,7 @@ export default function BrowseRestaurants() {
   const [restaurants, setRestaurants] = useState([]);
   const [userQuery, setUserQuery] = useState('');
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const router = useRouter();
 
   // Fetch restaurant data
   useEffect(() => {
@@ -35,7 +37,7 @@ export default function BrowseRestaurants() {
     <div className="w-screen h-fit min-h-screen bg-black">
       {/* Gray section */}
       <div className="sticky top-0 z-10 w-full bg-dark-jungle-green h-[35.7vh] flex flex-col px-5 py-6 justify-between">
-        <BackButton clickHandler={() => {}} />
+        <BackButton clickHandler={() => router.back()} />
         <h1 className="text-[2.25rem] font-bold text-white">
           Restaurants
         </h1>
@@ -45,7 +47,7 @@ export default function BrowseRestaurants() {
       <div className="w-screen h-fit min-h-[64.3%] bg-black px-5 pt-4 pb-[75px] text-white">
         <h3 className="text-lg font-semibold p-0">Near you</h3>
         <p className="text-sm">
-          {`${filteredRestaurants.length} Restaurant${filteredRestaurants.length > 1 ? 's' : ''} near you`}
+          {`${filteredRestaurants.length} Restaurant${filteredRestaurants.length !== 1 ? 's' : ''} near you`}
         </p>
         {!filteredRestaurants.length && null ||
           filteredRestaurants.map((restaurant, i) => (
