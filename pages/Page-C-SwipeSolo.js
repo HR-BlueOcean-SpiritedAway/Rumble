f60ca488c9e9d4945e9e34be77001dbeb4ca3032
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-05-12 12:27:40 -0700

    chore: cleans up page C

--- pages/Page-C-SwipeSolo.js
+++ pages/Page-C-SwipeSolo.js
@@ -5,178 +5,174 @@ import TinderCard from 'react-tinder-card';
 import React, { useState, useMemo, useRef, useEffect } from 'react';
 import axios from 'axios';
 
-// Below is for icons
-import setting from '../public/images/setting.svg';
-import logo_white from '../public/images/Rumble_white.svg';
-import cart from '../public/images/cart.svg';
-import single_user from '../public/images/single_user.svg';
-import restaurants from '../public/images/restaurants.svg';
-import list from '../public/images/list.svg';
-import addFriend from '../public/images/add_friend.svg';
-import like from '../public/images/heart.svg';
-import dislike from '../public/images/dislike.svg';
+// Components
+import BottomNavBar from '../components/BottomNavBar';
 
-// imported components
-import BottomNavBar from '../components/BottomNavBar.js';
-
-//authorization
+// User auth
 const { auth } = require('../firebase');
 const { useAuthState } = require('react-firebase-hooks/auth')
 
+// Icons
+import setting from '../public/images/setting.svg';
+import logo_white from '../public/images/Rumble_white.svg';
+import single_user from '../public/images/single_user.svg';
+import like from '../public/images/heart.svg';
+import dislike from '../public/images/dislike.svg';
+
+// Filter restaurants based on user preferences
+function filterRestaurants(array, preferences) {
+  const { cuisinePref, priceRange } = preferences;
+  let filtered = array;
+  if (cuisinePref && cuisinePref !== 'All')
+    filtered = filtered.filter(x => x.cuisine === cuisinePref);
+  if (priceRange)
+    filtered = filtered.filter(x => x.priceRange === priceRange);
+  return filtered;
+}
+
+// Tag used to display restaurant information
+function Tag({ text }) {
+  return (
+    <div className="grid items-center rounded-2xl px-3 bg-dark-jungle-green">
+      <p className="font-bold text-star-dust-light text-sm">{text}</p>
+    </div>
+  );
+};
+
+// Page C component
 export default function RestaurantSwipeSolo () {
   const [currentIndex, setCurrentIndex] = useState(0)
-  const [lastDirection, setLastDirection] = useState('')
-  const [favorite, setFavorite] = useState([])
-  const [db, setDb] = useState([])
+  const [restaurants, setRestaurants] = useState([])
   const [rightSwipes, setRightSwipes] = useState([]);
-  const [currentPage, setCurrentPage] = useState('');
-  const [user, loading] = useAuthState(auth);
-
   const [userPref, setUserPref] = useState([]);
+  const [user] = useAuthState(auth);
 
-
+  // Fetch user preferences on initial page render
   useEffect(() => {
     axios.get(`/api/users/getSingleUserInfo/${user.uid}`)
-    .then(({data}) => {
-      console.log('data from users db', data)
-      const { cuisinePref, priceRange } = data;
-      setUserPref({ cuisinePref, priceRange })
+    .then(({ data }) => {
+      let { cuisinePref, priceRange } = data;
+      setUserPref({ cuisinePref, priceRange });
     })
-    .catch(console.error)
+    .catch(console.error);
   }, []);
 
+  // On initial render and/or when user preferences change, refilter the list
+  // of restaurants seen by the user
   useEffect(() => {
     axios.get('/api/restaurants/test')
       .then(({ data }) => {
-      setDb(filterRes(data, userPref))})
+        const filteredRestaurants = filterRestaurants(data, userPref);
+        setRestaurants(filteredRestaurants);
+        setCurrentIndex(Math.max(filteredRestaurants.length - 1, 0))
+      })
       .catch(console.error)
   }, [userPref]);
 
-  const filterRes = (array, obj) => {
-    let filtered = array.filter(
-      element => element.cuisine === obj.cuisinePref
-      && element.priceRange === obj.priceRange
-    )
-    return filtered;
-  }
-
+  // Automatically navigate to Page L if user has swiped right on 3 restaurants
   useEffect(() => {
-    setCurrentIndex(db.length ? db.length - 1 : 0)
-  }, [db]);
-
-  useEffect(() => {
-    if(rightSwipes.length === 3) {
-    Router.push('/Page-L-SelectedRestaurants');
-    }
+    if(rightSwipes.length === 3) Router.push('/Page-L-SelectedRestaurants');
   }, [rightSwipes])
 
   // used for outOfFrame closure
   const currentIndexRef = useRef(currentIndex)
   const childRefs = useMemo(
-    () =>
-      Array(db.length)
-        .fill(0)
-        .map((i) => React.createRef()),
-    [db.length]
-  )
+    () => Array(restaurants.length).fill(React.createRef()), 
+    [restaurants.length]
+  );
 
-  const updateCurrentIndex = (val) => {
-    setCurrentIndex(val)
-    currentIndexRef.current = val
+  // Decrease current index on swipe
+  const handleSwipe = (direction, index, res) => {
+    if (direction === 'right') {
+      setRightSwipes((prev) => [res].concat(prev));
+      axios.post('/api/users/addFavorite', { uid: user.uid, restaurantID: res.id})
+        .catch(console.error);
+    }
+
+    setCurrentIndex(index - 1);
+    currentIndexRef.current = index - 1;
   }
 
-  const canGoBack = currentIndex < db.length - 1
-  const canSwipe = currentIndex >= 0
-
-  // set last direction and decrease current index
-  const swiped = (direction, nameToDelete, index, res) => {
-    if(direction === 'right') {
-      setRightSwipes((prev) => {
-        return [res].concat(prev)});
-        axios.post('/api/users/addFavorite', {
-          uid: user.uid,
-          restaurantID: res.id
-        }).catch(console.error);
-      }
-
-    setLastDirection(direction);
-    updateCurrentIndex(index - 1);
-  }
-
-
-  const outOfFrame = (name, idx) => {
-    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current)
-    // handle the case in which go back is pressed before card goes outOfFrame
-    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard()
-    // TODO: when quickly swipe and restore multiple times the same card,
-    // it happens multiple outOfFrame events are queued and the card disappear
-    // during latest swipes. Only the last outOfFrame event should be considered valid
-  }
-
-  const swipe = async (dir) => {
-    if (canSwipe && currentIndex < db.length) {
-      await childRefs[currentIndex].current.swipe(dir) // Swipe the card!
+  // Programatically swipe on current card
+  const swipe = async (direction) => {
+    if ((currentIndex >= 0) && (currentIndex < restaurants.length)) {
+      await childRefs[currentIndex].current.swipe(direction);
     }
   }
 
-  // increase current index and show card
-  const goBack = async () => {
-    if (!canGoBack) return
-    const newIndex = currentIndex + 1
-    updateCurrentIndex(newIndex)
-    await childRefs[newIndex].current.restoreCard()
-  }
-
   return (
-    <div className="bg-dark-jungle-green h-screen bg-cover">
-      <div className="py-7 px-7 relative flex justify-around">
+    // Page container
+    <div className="bg-dark-jungle-green h-screen">
+      {/* Top section */}
+      <div className="py-7 px-7 flex justify-around">
         <Link href="/Page-G-Settings">
-          <Image width={30} height={30} alt="setting" src={setting}/>
+          <a>
+            <Image width={30} height={30} alt="setting" src={setting}/>
+          </a>
         </Link>
-          <Image width={105} height={40} alt="white logo" src={logo_white}/>
-          <Image width={30} height={30} alt="single user" src={single_user}/>
+        <Image width={105} height={40} alt="white logo" src={logo_white}/>
+        <Image width={30} height={30} alt="single user" src={single_user}/>
       </div>
-      <div className="container">
-            {db.length === 0 ? null : db.map((res, index) => (
-              <TinderCard
-                ref={childRefs[index]}
-                className='absolute'
-                key={res.restaurantName}
-                onSwipe={(dir) => {
-                  swiped(dir, res.restaurantName, index, res);
-                }}
-                onCardLeftScreen={() => outOfFrame(res.restaurantName, index)}
-              >
-                <div
-                  style={{ backgroundImage: `url(${res?.dishes?.[0]?.photoURL}`  }}
-                  className="bg-gradient-to-t from-black relative bg-cover bg-center w-[375px] h-[65vh] rounded-[30px] flex flex-col justify-end"
-                >
-                  <h3 className="text-center text-white text-[2.25rem] font-bold">{res.restaurantName}</h3>
-                  <h4 className="text-center text-white text-[1.4rem]">{res.cuisine} Restaurant</h4>
 
-                  <div className="bg-white mx-4 px-2 py-6 rounded-[25px] mt-5 mb-5">
-                      <div className="flex justify-around pb-2">
-                        <div className="text-limed-spruce font-bold">About</div>
-                        <div className="text-star-dust-light bg-dark-jungle-green mx-4 px-2 rounded-[25px] text-[14px]">{res.priceRange}</div>
-                        <div className="text-star-dust-light bg-dark-jungle-green mx-4 px-2 rounded-[25px] text-[14px]">Fast</div>
-                        <div className="text-star-dust-light bg-dark-jungle-green mx-4 px-2 rounded-[25px] text-[14px]">12 miles</div>
-                      </div>
-                      <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      {/* Swipe Area */}
+      <div className="flex flex-col justify-between items-center">
+        {restaurants.length === 0 ? null : restaurants.map((res, index) => (
+          <TinderCard
+            ref={childRefs[index]}
+            className="absolute"
+            key={index}
+            onSwipe={(direction) => handleSwipe(direction, index, res)}
+            onCardLeftScreen={() => {}}
+          >
+            <div
+              style={{ backgroundImage: `url(${res?.dishes?.[0]?.photoURL}`  }}
+              className="bg-gradient-to-t from-black relative bg-cover bg-center 
+                        w-[360px] h-[65vh] rounded-[30px] flex flex-col justify-end"
+            >
+              <h3 className="text-center text-white text-[2.25rem] font-bold">
+                {res.restaurantName}
+              </h3>
+              <h4 className="text-center text-white text-[1.4rem]">
+                {res.cuisine} Restaurant
+              </h4>
+
+              {/* Restaurant info card */}
+              <div className="bg-white mx-4 px-2 py-6 rounded-[25px] mt-5 mb-5">
+                  <div className="flex justify-around pb-2">
+                    <div className="text-limed-spruce font-bold">About</div>
+                    <Tag text={res.priceRange} />
+                    <Tag text="Fast" />
+                    <Tag text="12 miles" />
                   </div>
-                </div>
-              </TinderCard>
-            ))}
+                  <p className="text-[13px] text-center">
+                    {res.description || 'Lorem ipsum dolor sit amet'}
+                  </p>
+              </div>
 
-        <div className="absolute bottom-[80px] left-[125px] flex space-x-[3rem]">
-          <div className="bg-white w-[40px] h-[40px] rounded-[30px] grid items-center" onClick={() => swipe('left')}>
+            </div>
+          </TinderCard>
+        ))}
+
+        {/* Like/Dislike Icons */}
+        <div className="absolute bottom-[80px] w-[360px] flex justify-evenly">
+          {/* Dislike Icon */}
+          <div 
+            className="bg-white w-[40px] h-[40px] rounded-[30px] grid items-center"
+            onClick={() => swipe('left')}
+          >
             <Image width={30} height={30} alt="dislike" src={dislike} />
           </div>
-          <div className="bg-reef-gold w-[40px] h-[40px] rounded-[30px] grid items-center" onClick={() => swipe('right')}>
+          {/* Like Icon */}
+          <div
+            className="bg-sunset-orange w-[40px] h-[40px] rounded-[30px] grid items-center"
+            onClick={() => swipe('right')}
+          >
             <Image width={30} height={30} alt="like" src={like}/>
           </div>
         </div>
       </div>
 
+      {/* Bottom Nav Bar */}
       <BottomNavBar />
     </div>
   );
